a75dc2914fa0c0d1f7691c8999a9b9c4dd58867c
秦 道平 <reynoldqin@gmail.com>
2014-04-08 14:11:26 +0800

    Initial Commit

---
+++ WKLocationManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WKLocationManager
+//
+//  Created by 秦 道平 on 14-4-8.
+//  Copyright (c) 2014年 秦 道平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WKLocationManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WKLocationManager
+//
+//  Created by 秦 道平 on 14-4-8.
+//  Copyright (c) 2014年 秦 道平. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
